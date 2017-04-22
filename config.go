3447aad5b9150f9d9fda8ef4226b1775c2c47efe
coderd <likesimplemail@gmail.com>
2017-04-23 01:09:44 +0800

    Added: many methods

--- config.go
+++ config.go
@@ -2,7 +2,18 @@ package gconfig
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"reflect"
+	"regexp"
+)
+
+const (
+	ErrKeyNotFound        = "gconfig: key '%s' is not found"
+	ErrNotBool            = "gconfig: key '%s' is not a bool"
+	ErrNotFloat64         = "gconfig: key '%s' is not a float64"
+	ErrNotString          = "gconfig: key '%s' is not a string"
+	ErrNotMapStringString = "gconfig: key '%s' is not a map[string]string"
 )
 
 type ConfigFile struct {
@@ -16,6 +27,10 @@ func LoadJsonFile(file string) (*ConfigFile, error) {
 		return nil, err
 	}
 
+	// Erase comments
+	re := regexp.MustCompile("//.*?\n")
+	content = re.ReplaceAll(content, []byte("\n"))
+
 	var data map[string]interface{}
 	err = json.Unmarshal(content, &data)
 	if err != nil {
@@ -28,4 +43,247 @@ func LoadJsonFile(file string) (*ConfigFile, error) {
 	}
 
 	return configFile, nil
+}
+
+func (c *ConfigFile) Get(key string) (interface{}, error) {
+	if v, ok := c.data[key]; ok {
+		return v, nil
+	}
+
+	return nil, fmt.Errorf(ErrKeyNotFound, key)
+}
+
+func (c *ConfigFile) Bool(key string) (bool, error) {
+	v, err := c.Get(key)
+	if err != nil {
+		return false, err
+	}
+
+	if value, ok := v.(bool); ok {
+		return value, nil
+	} else {
+		return false, fmt.Errorf(ErrNotBool, key)
+	}
+}
+
+func (c *ConfigFile) Float64(key string) (float64, error) {
+	v, err := c.Get(key)
+	if err != nil {
+		return 0.0, err
+	}
+
+	if value, ok := v.(float64); ok {
+		return value, nil
+	} else {
+		return 0.0, fmt.Errorf(ErrNotFloat64, key)
+	}
+}
+
+func (c *ConfigFile) String(key string) (string, error) {
+	v, err := c.Get(key)
+	if err != nil {
+		return "", err
+	}
+
+	if value, ok := v.(string); ok {
+		return value, nil
+	} else {
+		return "", fmt.Errorf(ErrNotString, key)
+	}
+}
+
+func (c *ConfigFile) MapStringBool(key string) (map[string]bool, error) {
+	v, err := c.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if value, ok := v.(map[string]interface{}); ok {
+		return MapStringBool(value)
+	} else {
+		return nil, fmt.Errorf(ErrNotMapStringString, key)
+	}
+}
+
+func (c *ConfigFile) MapStringFloat64(key string) (map[string]float64, error) {
+	v, err := c.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if value, ok := v.(map[string]interface{}); ok {
+		return MapStringFloat64(value)
+	} else {
+		return nil, fmt.Errorf(ErrNotMapStringString, key)
+	}
+}
+
+func (c *ConfigFile) MapStringString(key string) (map[string]string, error) {
+	v, err := c.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if value, ok := v.(map[string]interface{}); ok {
+		return MapStringString(value)
+	} else {
+		return nil, fmt.Errorf(ErrNotMapStringString, key)
+	}
+}
+
+func (c *ConfigFile) MustBool(key string) bool {
+	v, err := c.Bool(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c *ConfigFile) MustFloat64(key string) float64 {
+	v, err := c.Float64(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c *ConfigFile) MustString(key string) string {
+	v, err := c.String(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c *ConfigFile) MustMapStringBool(key string) map[string]bool {
+	v, err := c.MapStringBool(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c *ConfigFile) MustMapStringFloat64(key string) map[string]float64 {
+	v, err := c.MapStringFloat64(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c *ConfigFile) MustMapStringString(key string) map[string]string {
+	v, err := c.MapStringString(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c *ConfigFile) AlwaysBool(key string, defaultVal ...bool) bool {
+	v, err := c.Bool(key)
+	if err != nil && len(defaultVal) > 0 {
+		return defaultVal[0]
+	}
+
+	return v
+}
+
+func (c *ConfigFile) AlwaysFloat64(key string, defaultVal ...float64) float64 {
+	v, err := c.Float64(key)
+	if err != nil && len(defaultVal) > 0 {
+		return defaultVal[0]
+	}
+
+	return v
+}
+
+func (c *ConfigFile) AlwaysString(key string, defaultVal ...string) string {
+	v, err := c.String(key)
+	if err != nil && len(defaultVal) > 0 {
+		return defaultVal[0]
+	}
+
+	return v
+}
+
+func (c *ConfigFile) AlwaysMapStringBool(key string, defaultVal ...map[string]bool) map[string]bool {
+	v, err := c.MapStringBool(key)
+	if err != nil && len(defaultVal) > 0 {
+		return defaultVal[0]
+	}
+
+	return v
+}
+
+func (c *ConfigFile) AlwaysMapStringFloat64(key string, defaultVal ...map[string]float64) map[string]float64 {
+	v, err := c.MapStringFloat64(key)
+	if err != nil && len(defaultVal) > 0 {
+		return defaultVal[0]
+	}
+
+	return v
+}
+
+func (c *ConfigFile) AlwaysMapStringString(key string, defaultVal ...map[string]string) map[string]string {
+	v, err := c.MapStringString(key)
+	if err != nil && len(defaultVal) > 0 {
+		return defaultVal[0]
+	}
+
+	return v
+}
+
+// An InvalidPointerError describes an invalid argument passed to Must and Always.
+type InvalidPointerError struct {
+	Type reflect.Type
+}
+
+func (i *InvalidPointerError) Error() string {
+	if i.Type == nil {
+		return "gconfig: set (nil)"
+	}
+
+	if i.Type.Kind() != reflect.Ptr {
+		return "gconfig: set(non-pointer " + i.Type.String() + ")"
+	}
+	return "gconfig: set(nil " + i.Type.String() + ")"
+}
+
+func set(pointer interface{}, v interface{}) error {
+	pointerReflectV := reflect.ValueOf(pointer)
+	pointerReflectT := reflect.TypeOf(pointer)
+	if pointerReflectV.Kind() != reflect.Ptr || pointerReflectV.IsNil() {
+		return &InvalidPointerError{pointerReflectT}
+	}
+
+	vReflectV := reflect.ValueOf(v)
+	vReflectT := reflect.TypeOf(v)
+
+	pointerElem := pointerReflectV.Elem()
+	pointerElemT := pointerElem.Type()
+
+	if pointerElemT != vReflectT {
+		return fmt.Errorf("gconfig: set() pointer's element is a `%s`, not a `%s`", vReflectT.String(), pointerElemT.String())
+	}
+
+	pointerElem.Set(vReflectV)
+
+	return nil
+}
+
+func (c *ConfigFile) Must(key string, value interface{}) {
+	v, err := c.Get(key)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = set(value, v); err != nil {
+		panic(err)
+	}
 }
--- config_test.go
+++ config_test.go
@@ -1,14 +1,196 @@
 package gconfig
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestLoadJsonFile(t *testing.T) {
-	testFile := "testdata/config.json"
+var testFile = "testdata/config.json"
 
-	_, err := LoadJsonFile(testFile)
-	if err != nil {
-		t.Errorf("LoadJsonFile '%s' failed: %s", testFile, err.Error())
-	}
+func TestLoadJsonFile(t *testing.T) {
+	var err error
+
+	configFile, err := LoadJsonFile(testFile)
+	assert.Nil(t, err)
+
+	var (
+		b   bool
+		i   float64
+		f   float64
+		s   string
+		msb map[string]bool
+		msf map[string]float64
+		mss map[string]string
+	)
+
+	// Test correct cases
+	// ========================================================================
+
+	// Test type-getting
+	b, err = configFile.Bool("bool")
+	assert.Nil(t, err)
+	assert.True(t, b)
+
+	i, err = configFile.Float64("int")
+	assert.Nil(t, err)
+	assert.Equal(t, 1.0, i)
+
+	f, err = configFile.Float64("float64")
+	assert.Nil(t, err)
+	assert.Equal(t, 1.01, f)
+
+	s, err = configFile.String("string")
+	assert.Nil(t, err)
+	assert.Equal(t, "foo", s)
+
+	_, err = configFile.MapStringBool("map_string_bool")
+	assert.Nil(t, err)
+
+	_, err = configFile.MapStringFloat64("map_string_float64")
+	assert.Nil(t, err)
+
+	_, err = configFile.MapStringString("map_string_string")
+	assert.Nil(t, err)
+
+	// Test must-getting
+	b = configFile.MustBool("bool")
+	assert.True(t, b)
+
+	i = configFile.MustFloat64("int")
+	assert.Equal(t, 1.0, i)
+
+	f = configFile.MustFloat64("float64")
+	assert.Equal(t, 1.01, f)
+
+	s = configFile.MustString("string")
+	assert.Equal(t, "foo", s)
+
+	assert.NotPanics(t, func() {
+		configFile.MustMapStringBool("map_string_bool")
+	})
+
+	assert.NotPanics(t, func() {
+		configFile.MustMapStringFloat64("map_string_float64")
+	})
+
+	assert.NotPanics(t, func() {
+		configFile.MustMapStringString("map_string_string")
+	})
+
+	// Test always-getting
+	b = configFile.AlwaysBool("bool")
+	assert.True(t, b)
+
+	i = configFile.AlwaysFloat64("int")
+	assert.Equal(t, 1.0, i)
+
+	f = configFile.AlwaysFloat64("float64")
+	assert.Equal(t, 1.01, f)
+
+	s = configFile.AlwaysString("string")
+	assert.Equal(t, "foo", s)
+
+	msb = configFile.AlwaysMapStringBool("map_string_bool")
+	assert.True(t, len(msb) > 0)
+
+	msf = configFile.AlwaysMapStringFloat64("map_string_float64")
+	assert.True(t, len(msf) > 0)
+
+	mss = configFile.AlwaysMapStringString("map_string_string")
+	assert.True(t, len(mss) > 0)
+
+	// Test others
+	f = 0.0
+	configFile.Must("float64", &f)
+	assert.Equal(t, 1.01, f)
+
+	// Test failed cases
+	// ========================================================================
+
+	// Test type-getting
+	b, err = configFile.Bool("int")
+	assert.NotNil(t, err)
+	assert.False(t, b)
+
+	i, err = configFile.Float64("bool")
+	assert.NotNil(t, err)
+	assert.Equal(t, 0.0, i)
+
+	f, err = configFile.Float64("string")
+	assert.NotNil(t, err)
+	assert.Equal(t, 0.0, f)
+
+	s, err = configFile.String("float64")
+	assert.NotNil(t, err)
+	assert.Equal(t, "", s)
+
+	_, err = configFile.MapStringBool("map_string_float64")
+	assert.NotNil(t, err)
+
+	_, err = configFile.MapStringFloat64("map_string_bool")
+	assert.NotNil(t, err)
+
+	_, err = configFile.MapStringString("map_string_float64")
+	assert.NotNil(t, err)
+
+	// Test must-getting
+	assert.Panics(t, func() {
+		configFile.MustBool("int")
+	})
+
+	assert.Panics(t, func() {
+		configFile.MustFloat64("bool")
+	})
+
+	assert.Panics(t, func() {
+		configFile.MustFloat64("string")
+	})
+
+	assert.Panics(t, func() {
+		configFile.MustString("float64")
+	})
+
+	assert.Panics(t, func() {
+		configFile.MustMapStringBool("map_string_float64")
+	})
+
+	assert.Panics(t, func() {
+		configFile.MustMapStringFloat64("map_string_bool")
+	})
+
+	assert.Panics(t, func() {
+		configFile.MustMapStringString("map_string_float64")
+	})
+
+	// Test always-getting without default values
+	b = configFile.AlwaysBool("int")
+	assert.False(t, b)
+
+	i = configFile.AlwaysFloat64("bool")
+	assert.Equal(t, 0.0, i)
+
+	f = configFile.AlwaysFloat64("string")
+	assert.Equal(t, 0.0, f)
+
+	s = configFile.AlwaysString("float64")
+	assert.Equal(t, "", s)
+
+	// Test always-getting with default values
+	b = configFile.AlwaysBool("int", true)
+	assert.True(t, b)
+
+	i = configFile.AlwaysFloat64("bool", 1.0)
+	assert.Equal(t, 1.0, i)
+
+	f = configFile.AlwaysFloat64("string", 1.1)
+	assert.Equal(t, 1.1, f)
+
+	s = configFile.AlwaysString("float64", "bar")
+	assert.Equal(t, "bar", s)
+
+	// Test others
+	f = 0.0
+	assert.Panics(t, func() {
+		configFile.Must("string", &f)
+	})
 }
